6c08a2d8dc3dea2ce39f0eca51a3e4a48f2119ec
xiaoyu01 <xiaoyu01@njfu.edu.cn>
2023-12-20 22:17:43 +0800

    Update NTQQ

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
+import json
 import os
-import asyncio, json
 from enum import IntEnum
-from typing import List, Tuple, Any, Dict
-from utils.standardPlugin import NotPublishedException
-from utils.basicConfigs import APPLY_GROUP_ID, APPLY_GUILD_ID, BACKEND, BACKEND_TYPE
-from utils.configsLoader import createApplyGroupsSql, loadApplyGroupId
-from utils.accountOperation import create_account_sql
-from utils.standardPlugin import (
-    StandardPlugin, PluginGroupManager, EmptyPlugin,
-    PokeStandardPlugin, AddGroupStandardPlugin, 
-    EmptyAddGroupPlugin,GuildStandardPlugin
-)
-from utils.sqlUtils import createBotDataDb
-from utils.configAPI import createGlobalConfig, removeInvalidGroupConfigs
-from utils.basicEvent import get_group_list, warning, set_friend_add_request, set_group_add_request
-from utils.messageChain import MessageChain
+from typing import List, Any, Dict
+from websocket_server import WebsocketServer
+from plugins.greetings import MorningGreet, NightGreet
 
 from plugins.autoRepoke import AutoRepoke
-from plugins.faq_v2 import MaintainFAQ, AskFAQ, HelpFAQ
-from plugins.groupCalendar import GroupCalendarHelper, GroupCalendarManager
-from plugins.greetings import MorningGreet, NightGreet
 from plugins.checkCoins import CheckCoins, AddAssignedCoins, CheckTransactions
-from plugins.superEmoji import FirecrackersFace, FireworksFace, BasketballFace, HotFace
-from plugins.news import ShowNews, YesterdayNews, UpdateNewsAndReport
+from plugins.faq_v2 import MaintainFAQ, AskFAQ, HelpFAQ
 from plugins.hotSearch import WeiboHotSearch, BaiduHotSearch, ZhihuHotSearch
+from plugins.news import ShowNews, YesterdayNews, UpdateNewsAndReport
 from plugins.signIn import SignIn
-from plugins.thanksLUB import ThanksLUB
-from plugins.sjmcStatus_v2 import ShowSjmcStatus
-from plugins.sjmcStatus_v4 import ShowMcStatus, McStatusAddServer, McStatusRemoveServer, McStatusSetFooter, McStatusRemoveFooter
+from plugins.sjmcStatus_v4 import ShowMcStatus, McStatusAddServer, McStatusRemoveServer, McStatusSetFooter, \
+    McStatusRemoveFooter
+from plugins.superEmoji import FirecrackersFace, FireworksFace, BasketballFace, HotFace
+from utils.accountOperation import create_account_sql
+from utils.basicConfigs import APPLY_GROUP_ID, APPLY_GUILD_ID
+from utils.basicEvent import warning, set_friend_add_request, set_group_add_request
+from utils.configAPI import createGlobalConfig
+from utils.configsLoader import createApplyGroupsSql, loadApplyGroupId
+from utils.messageChain import MessageChain
+from utils.sqlUtils import createBotDataDb
+from utils.standardPlugin import NotPublishedException
+from utils.standardPlugin import (
+    StandardPlugin, PluginGroupManager, EmptyPlugin,
+    PokeStandardPlugin, AddGroupStandardPlugin,
+    GuildStandardPlugin
+)
+
 try:
-    from plugins.mua import (MuaAnnHelper, MuaAnnEditor, 
-        MuaTokenBinder, MuaTokenUnbinder, MuaTokenEmpower,
-        MuaTokenLister, MuaNotice, MuaQuery, MuaAbstract,
-        MuaGroupBindTarget, MuaGroupUnbindTarget, MuaGroupAnnFilter)
+    from plugins.mua import (MuaAnnHelper, MuaAnnEditor,
+                             MuaTokenBinder, MuaTokenUnbinder, MuaTokenEmpower,
+                             MuaTokenLister, MuaNotice, MuaQuery, MuaAbstract,
+                             MuaGroupBindTarget, MuaGroupUnbindTarget, MuaGroupAnnFilter)
 except NotPublishedException as e:
     print('mua plugins not imported: {}'.format(e))
     MuaAnnHelper, MuaAnnEditor = EmptyPlugin, EmptyPlugin
@@ -43,32 +43,23 @@ except NotPublishedException as e:
     MuaTokenLister = EmptyPlugin
 from plugins.roulette import RoulettePlugin
 from plugins.lottery import LotteryPlugin
-from plugins.show2cyPic import Show2cyPIC, ShowSePIC
 from plugins.help_v2 import ShowHelp, ShowStatus, ServerMonitor
-from plugins.groupBan import GroupBan, UserBan, BanImplement, GetBanList
+from plugins.groupBan import UserBan, BanImplement, GetBanList
 from plugins.privateControl import PrivateControl, LsGroup, GroupApply, HelpInGroup
 from plugins.bilibiliSubscribe_v2 import BilibiliSubscribe, BilibiliSubscribeHelper
-from plugins.chatWithAnswerbook import ChatWithAnswerbook
 from plugins.getPermission import GetPermission, AddPermission, DelPermission, ShowPermission, AddGroupAdminToBotAdmin
-# from plugins.goBang import GoBangPlugin
 from plugins.messageRecorder import GroupMessageRecorder
 from plugins.addGroupRecorder import AddGroupRecorder
-from plugins.bilibiliLive import GetBilibiliLive, BilibiliLiveMonitor
-from plugins.deprecated.sjmcLive import GetSjmcLive
+from plugins.fileRecorder import GroupFileRecorder
 # from plugins.advertisement import McAdManager
-from plugins.groupActReport import ActReportPlugin, ActRankPlugin, YourActReportPlugin
-from plugins.groupWordCloud import wordCloudPlugin, GenWordCloud, GenPersonWordCloud
 from plugins.randomNum import TarotRandom, RandomNum, ThreeKingdomsRandom
-from plugins.cchess import ChineseChessPlugin, ChineseChessHelper
-# from plugins.song import ChooseSong # API坏了
-from plugins.clearRecord import ClearRecord, RestoreRecord
 from plugins.bilibiliLive import GetBilibiliLive, BilibiliLiveMonitor
-from plugins.emojiKitchen import EmojiKitchen
 from plugins.leetcode import ShowLeetcode, LeetcodeReport
-from plugins.abstract import MakeAbstract
 from plugins.eavesdrop import Eavesdrop
 
 from plugins.gocqWatchDog import GocqWatchDog
+
+
 ###### end not published plugins
 
 def sqlInit():
@@ -80,91 +71,78 @@ def sqlInit():
     loadApplyGroupId()
     # removeInvalidGroupConfigs() # it may danger, consider change it to add tag
 
-sqlInit() # put this after import
+
+sqlInit()  # put this after import
 
 ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 RESOURCES_PATH = os.path.join(ROOT_PATH, "resources")
 
 # 特殊插件需要复用的放在这里
-helper = ShowHelp() # 帮助插件
-helperForPrivateControl = HelpInGroup() # BOT管理员查看群聊功能开启情况插件
+helper = ShowHelp()  # 帮助插件
+helperForPrivateControl = HelpInGroup()  # BOT管理员查看群聊功能开启情况插件
 gocqWatchDog = GocqWatchDog(60)
-groupMessageRecorder = GroupMessageRecorder() # 群聊消息记录插件
+groupMessageRecorder = GroupMessageRecorder()  # 群聊消息记录插件
 getXjmcBilibili = GetBilibiliLive(30143840, '西交MC团队', '-xjmclive')
 getFdmcBilibili = GetBilibiliLive(24716629, '基岩社', '-fdmclive')
 banImpl = BanImplement()
 # BilibiliLiveMonitor(30539032, 'MUA', 'mualive')
 # GetBilibiliLive(30539032, 'MUA', '-mualive')
 
-GroupPluginList:List[StandardPlugin]=[ # 指定群启用插件
-    groupMessageRecorder, banImpl, 
-    helper,ShowStatus(),ServerMonitor(), # 帮助
-    GetPermission(), ThanksLUB(), Eavesdrop(),
+GroupPluginList: List[StandardPlugin] = [  # 指定群启用插件
+    groupMessageRecorder, banImpl,
+    helper, ShowStatus(), ServerMonitor(),  # 帮助
+    GetPermission(), Eavesdrop(),
     PluginGroupManager([AddPermission(), DelPermission(), ShowPermission(), AddGroupAdminToBotAdmin(),
-                        UserBan(banImpl), GetBanList()], 'permission'), # 权限
-    PluginGroupManager([AskFAQ(), MaintainFAQ(), HelpFAQ()],'faq'), # 问答库与维护
-    PluginGroupManager([GroupCalendarHelper(), GroupCalendarManager()], 'calendar'),
-    PluginGroupManager([MorningGreet(), NightGreet()], 'greeting'), # 早安晚安
-    PluginGroupManager([CheckCoins(), AddAssignedCoins(),CheckTransactions()],'money'), # 查询金币,查询记录,增加金币（管理员）
-    PluginGroupManager([FireworksFace(), FirecrackersFace(), BasketballFace(), HotFace(), ], 'superemoji'), # 超级表情
+                        UserBan(banImpl), GetBanList()], 'permission'),  # 权限
+    PluginGroupManager([AskFAQ(), MaintainFAQ(), HelpFAQ()], 'faq'),  # 问答库与维护
+    PluginGroupManager([MorningGreet(), NightGreet()], 'greeting'),  # 早安晚安
+    PluginGroupManager([CheckCoins(), AddAssignedCoins(), CheckTransactions()], 'money'),  # 查询金币,查询记录,增加金币（管理员）
+    PluginGroupManager([FireworksFace(), FirecrackersFace(), BasketballFace(), HotFace(), ], 'superemoji'),  # 超级表情
     PluginGroupManager([ShowNews(), YesterdayNews(),
-                        PluginGroupManager([UpdateNewsAndReport()], 'newsreport')],'news'),  # 新闻
-    PluginGroupManager([WeiboHotSearch(), BaiduHotSearch(), ZhihuHotSearch(),], 'hotsearch'),
+                        PluginGroupManager([UpdateNewsAndReport()], 'newsreport')], 'news'),  # 新闻
+    PluginGroupManager([WeiboHotSearch(), BaiduHotSearch(), ZhihuHotSearch(), ], 'hotsearch'),
     PluginGroupManager([SignIn()], 'signin'),  # 签到
-    PluginGroupManager([ShowSjmcStatus(), GetSjmcLive(), getFdmcBilibili, getXjmcBilibili,
-                        PluginGroupManager([BilibiliLiveMonitor(25567444, '交大MC社', 'mclive'),
-                                            BilibiliLiveMonitor(24716629, '基岩社', 'mclive'),
-                                            BilibiliLiveMonitor(30143840, '西交MC团队', 'mclive')], 'mclive'),
-                        ], 'sjmc'), #MC社服务
-    PluginGroupManager([ShowMcStatus(), McStatusAddServer(), McStatusRemoveServer(), McStatusSetFooter(), McStatusRemoveFooter()
-                        ], 'mcs'), #MC服务器列表for MUA
+    PluginGroupManager(
+        [ShowMcStatus(), McStatusAddServer(), McStatusRemoveServer(), McStatusSetFooter(), McStatusRemoveFooter()
+         ], 'mcs'),  # MC服务器列表for MUA
     PluginGroupManager([MuaQuery(), MuaAbstract(), MuaAnnHelper(), MuaAnnEditor(),
                         MuaTokenBinder(), MuaTokenUnbinder(), MuaTokenEmpower(), MuaTokenLister(),
                         MuaGroupBindTarget(), MuaGroupUnbindTarget(), MuaGroupAnnFilter(),
                         GetBilibiliLive(30539032, 'MUA', '-mualive'),
                         PluginGroupManager([MuaNotice()], 'muanotice'),
-                        PluginGroupManager([BilibiliLiveMonitor(30539032, 'MUA', 'mualive'),], 'mualive'),
-                        ], 'mua'), #MC高校联盟服务
-    PluginGroupManager([RoulettePlugin()],'roulette'), # 轮盘赌
-    PluginGroupManager([LotteryPlugin()],'lottery'), # 彩票 TODO
-    # PluginGroupManager([GoBangPlugin()],'gobang'),
-    PluginGroupManager([Show2cyPIC()], 'anime'), #ShowSePIC(), # 来点图图，来点涩涩(关闭)
-    PluginGroupManager([ChatWithAnswerbook(), ], 'chat'), # 答案之书
-    PluginGroupManager([ActReportPlugin(), YourActReportPlugin(), ActRankPlugin(), wordCloudPlugin(),
-                        ClearRecord(), RestoreRecord(), GenPersonWordCloud(),
-                        PluginGroupManager([GenWordCloud()], 'wcdaily')], 'actreport'), #水群报告
+                        PluginGroupManager([BilibiliLiveMonitor(30539032, 'MUA', 'mualive'), ], 'mualive'),
+                        ], 'mua'),  # MC高校联盟服务
+    PluginGroupManager([RoulettePlugin()], 'roulette'),  # 轮盘赌
+    PluginGroupManager([LotteryPlugin()], 'lottery'),  # 彩票 TODO
     PluginGroupManager([RandomNum(), ThreeKingdomsRandom(), TarotRandom()], 'random'),
     PluginGroupManager([BilibiliSubscribeHelper(), BilibiliSubscribe()], 'bilibili'),
-    PluginGroupManager([ChineseChessPlugin(), ChineseChessHelper()], 'cchess'),
     PluginGroupManager([ShowLeetcode(), LeetcodeReport()], 'leetcode')
 ]
-PrivatePluginList:List[StandardPlugin]=[ # 私聊启用插件
-    helper, ThanksLUB(),
-    ShowStatus(),ServerMonitor(),
+PrivatePluginList: List[StandardPlugin] = [  # 私聊启用插件
+    helper,
+    ShowStatus(), ServerMonitor(),
     LsGroup(), GroupApply(), PrivateControl(), helperForPrivateControl,
-    CheckCoins(),AddAssignedCoins(),CheckTransactions(),
+    CheckCoins(), AddAssignedCoins(), CheckTransactions(),
     ShowNews(), YesterdayNews(),
     MorningGreet(), NightGreet(),
-    SignIn(), 
-    MuaAbstract(), MuaQuery(), MuaAnnHelper(), MuaAnnEditor(), 
+    SignIn(),
+    MuaAbstract(), MuaQuery(), MuaAnnHelper(), MuaAnnEditor(),
     MuaTokenBinder(), MuaTokenUnbinder(), MuaTokenEmpower(), MuaTokenLister(),
     LotteryPlugin(),
-    Show2cyPIC(), #ShowSePIC(),
-    GetSjmcLive(), getFdmcBilibili, getXjmcBilibili,
     RandomNum(), ThreeKingdomsRandom(), TarotRandom(),
 ]
-GuildPluginList:List[GuildStandardPlugin] = [
+GuildPluginList: List[GuildStandardPlugin] = [
 
 ]
-GroupPokeList:List[PokeStandardPlugin] = [
-    AutoRepoke(), # 自动回复拍一拍
+GroupPokeList: List[PokeStandardPlugin] = [
+    AutoRepoke(),  # 自动回复拍一拍
 ]
-AddGroupVerifyPluginList:List[AddGroupStandardPlugin] = [
-    AddGroupRecorder(), # place this plugin to the first place
+AddGroupVerifyPluginList: List[AddGroupStandardPlugin] = [
+    AddGroupRecorder(),  # place this plugin to the first place
 ]
 helper.updatePluginList(GroupPluginList, PrivatePluginList)
 helperForPrivateControl.setPluginList(GroupPluginList)
-app = Flask(__name__)
+
 
 class NoticeType(IntEnum):
     NoProcessRequired = 0
@@ -178,12 +156,13 @@ class NoticeType(IntEnum):
     PrivateMessage = 21
     PrivatePoke = 22
     PrivateRecall = 23
-    AddGroup = 31       # 有人要求加入自己的群
+    AddGroup = 31  # 有人要求加入自己的群
     AddPrivate = 32
-    AddGroupInvite = 33 # 有人邀请自己加入新群
+    AddGroupInvite = 33  # 有人邀请自己加入新群
     GuildMessage = 41
 
-def eventClassify(json_data: dict)->NoticeType: 
+
+def eventClassify(json_data: dict) -> NoticeType:
     """事件分类"""
     if json_data['post_type'] == 'meta_event' and json_data['meta_event_type'] == 'heartbeat':
         return NoticeType.GocqHeartBeat
@@ -220,21 +199,20 @@ def eventClassify(json_data: dict)->NoticeType:
     else:
         return NoticeType.NoProcessRequired
 
-def onMessageReceive(message:str)->str:
-    data:Dict[str,Any] = json.loads(message)
-    # 筛选并处理指定事件
-    flag=eventClassify(data)
 
-    # 消息格式转换
-    if BACKEND == BACKEND_TYPE.LAGRANGE and 'message' in data.keys():
+def post_data(_0, _1, message):
+    # 获取事件上报
+    data: Dict[str, Any] = json.loads(message)
+    # 筛选并处理指定事件
+    flag = eventClassify(data)
+    # 群消息处理
+    if 'message' in data.keys():
         msgChain = MessageChain(data['message'])
         msgOrigin = msgChain.toCqcode()
         msg = msgOrigin.strip()
         data['message_chain'] = data['message']
         data['message'] = msgOrigin
-
-    if flag==NoticeType.GroupMessage: # 群消息处理
-        msg = data['message'].strip()
+    if flag == NoticeType.GroupMessage:
         for event in GroupPluginList:
             event: StandardPlugin
             try:
@@ -250,26 +228,26 @@ def onMessageReceive(message:str)->str:
         for plugin in [groupMessageRecorder]:
             plugin.recallMessage(data)
     # 频道消息处理
-    elif flag == NoticeType.GuildMessage:
-        msg = data['message'].strip()
-        for plugin in GuildPluginList:
-            plugin: GuildStandardPlugin
-            try:
-                if plugin.judgeTrigger(msg, data):
-                    ret = plugin.executeEvent(msg, data)
-                    if ret != None:
-                        return ret
-            except TypeError as e:
-                warning("type error in main.py: {}\n\n{}".format(e, plugin))
-            except BaseException as e:
-                warning('base exception in main.py guild plugin: {}\n\n{}'.format(e, plugin))
+    # elif flag == NoticeType.GuildMessage:
+    #     msg = data['message'].strip()
+    #     for plugin in GuildPluginList:
+    #         plugin: GuildStandardPlugin
+    #         try:
+    #             if plugin.judgeTrigger(msg, data):
+    #                 ret = plugin.executeEvent(msg, data)
+    #                 if ret != None:
+    #                     return ret
+    #         except TypeError as e:
+    #             warning("type error in main.py: {}\n\n{}".format(e, plugin))
+    #         except BaseException as e:
+    #             warning('base exception in main.py guild plugin: {}\n\n{}'.format(e, plugin))
     # 私聊消息处理
     elif flag == NoticeType.PrivateMessage:
         # print(data)
-        msg=data['message'].strip()
+        msg = data['message'].strip()
         for event in PrivatePluginList:
             if event.judgeTrigger(msg, data):
-                if event.executeEvent(msg, data)!=None:
+                if event.executeEvent(msg, data) != None:
                     break
 
     elif flag == NoticeType.AddGroup:
@@ -279,61 +257,48 @@ def onMessageReceive(message:str)->str:
                     break
     # 上传文件处理
     elif flag == NoticeType.GroupUpload:
-        for event in []:
+        for event in [GroupFileRecorder()]:
             event.uploadFile(data)
     # 群内拍一拍回拍
-    elif flag==NoticeType.GroupPoke: 
+    elif flag == NoticeType.GroupPoke:
         for p in GroupPokeList:
             if p.judgeTrigger(data):
-                if p.pokeMessage(data)!=None:
+                if p.pokeMessage(data) != None:
                     break
-            
+
     # 自动加好友
-    elif flag==NoticeType.AddPrivate:
+    elif flag == NoticeType.AddPrivate:
         set_friend_add_request(data['flag'], True)
     # 自动通过加群邀请
-    elif flag==NoticeType.AddGroupInvite:
+    elif flag == NoticeType.AddGroupInvite:
         set_group_add_request(data['flag'], data['sub_type'], True)
-    elif flag==NoticeType.GocqHeartBeat:
+    elif flag == NoticeType.GocqHeartBeat:
         gocqWatchDog.feed()
     return "OK"
+
 
 def initCheck():
     # do some check
     for p in GroupPluginList:
         infoDict = p.getPluginInfo()
         assert 'name' in infoDict.keys() and 'description' in infoDict.keys() \
-            and 'commandDescription' in infoDict.keys() and 'usePlace' in infoDict.keys()
+               and 'commandDescription' in infoDict.keys() and 'usePlace' in infoDict.keys()
         if 'group' not in infoDict['usePlace']:
             print("plugin [{}] can not be used in group talk!".format(infoDict['name']))
             exit(1)
     for p in PrivatePluginList:
         infoDict = p.getPluginInfo()
         assert 'name' in infoDict.keys() and 'description' in infoDict.keys() \
-            and 'commandDescription' in infoDict.keys() and 'usePlace' in infoDict.keys()
+               and 'commandDescription' in infoDict.keys() and 'usePlace' in infoDict.keys()
         if 'private' not in infoDict['usePlace']:
             print("plugin [{}] can not be used in private talk!".format(infoDict['name']))
             exit(1)
     gocqWatchDog.start()
 
+
 if __name__ == '__main__':
     initCheck()
-    if BACKEND == BACKEND_TYPE.GOCQHTTP:
-        from flask import Flask, request
-        app = Flask(__name__)
-        @app.route('/', methods=["POST"])
-        def onMsgRecvGocq():
-            msg = request.get_data(as_text=True)
-            return onMessageReceive(msg)
-        app.run(host="127.0.0.1", port=5986)
-
-    elif BACKEND == BACKEND_TYPE.LAGRANGE:
-        from websocket_server import WebsocketServer
-        server = WebsocketServer("127.0.0.1", port=5706)
-        def onMsgRecvLag(_0, _1, msg):
-            onMessageReceive(msg)
-        server.set_fn_message_received(onMsgRecvLag)
-        print('-------------You can start Lagrange Now----------------')
-        server.run_forever()
-    else:
-        print('invalid backend type')
+    server = WebsocketServer("127.0.0.1", port=5706)
+    server.set_fn_message_received(post_data)
+    print('-------------You can start Lagrange Now----------------')
+    server.run_forever()
--- utils/basicConfigs.py
+++ utils/basicConfigs.py
@@ -1,76 +1,68 @@
-from PIL import Image, ImageDraw, ImageFont
 import os
-import enum
+
+from PIL import ImageFont
 
 # from utils.basicEvent import get_login_info
-HTTP_URL = "http://127.0.0.1:5700"  # go-cqhttp
+HTTP_URL = "ws://127.0.0.1:5705"  # Lagrange
 
 APPLY_GROUP_ID = []
 
-APPLY_GUILD_ID = []  # guild test
+APPLY_GUILD_ID = [
 
-MAIN_GUILD = {"guild_id": "", "channels": {}}  # guild test
+]
 
-JAC_COOKIE = ""  # for dekt
+MAIN_GUILD = {
 
-ROOT_ADMIN_ID = []
+}
 
-WARNING_ADMIN_ID = []
+JAC_COOKIE = ''
 
-MAIL_USER = ""  # watch dog mail to whom
-MAIL_PASS = ""
+ROOT_ADMIN_ID = [
+]
 
-# BOT_SELF_QQ=get_login_info()['user_id']
-BOT_SELF_QQ = 0
+assert len(ROOT_ADMIN_ID) > 0, '请至少指定一个用户为机器人ROOT'
+
+WARNING_ADMIN_ID = [
+]
+
+MAIL_USER = ''
+MAIL_PASS = ''
+
+BOT_SELF_QQ = 0  # bot自己qq号是多少
+assert BOT_SELF_QQ != None, 'BOT的QQ号是多少'
 
 VERSION_TXT = """version：muaLite版本，NJFU基于Little-UNIkeEN-Bot的muaLTS分支维护的精简版本
-本版本更新内容见文档： https://github.com/xiaoyu-success/Little-UNIkeEN-Bot-Lite/"""
+本版本更新内容见GitHub(可能需要科学上网)： https://github.com/xiaoyu-success/Little-UNIkeEN-Bot-Lite/
+国内自建Gitlab：https://codeserver.youkeda.com/xiaoyu01/little-unikeen-bot-lite/"""
 
-sqlConfig = {"host": "127.0.0.1", "user": "root", "passwd": ""}
+sqlConfig = {
+    'host': '127.0.0.1',
+    'user': 'root',
+    'passwd': ''  # bot的sql密码是多少
+}
+assert sqlConfig.get('passwd', None) != None, '请填入bot sql的摩玛'
 
 # 根路径与资源路径
 ROOT_PATH = os.path.dirname(os.path.realpath(__file__))[:-6]
-FONTS_PATH = "resources/fonts"
-IMAGES_PATH = "resources/images/"
-SAVE_TMP_PATH = "data/tmp"
+FONTS_PATH = 'resources/fonts'
+IMAGES_PATH = 'resources/images/'
+SAVE_TMP_PATH = 'data/tmp'
 os.makedirs(SAVE_TMP_PATH, exist_ok=True)
 
 # 画图颜色常量与文字
-BACK_CLR = {
-    "r": (255, 232, 236, 255),
-    "g": (219, 255, 228, 255),
-    "h": (234, 234, 234, 255),
-    "o": (254, 232, 199, 255),
-}
-FONT_CLR = {
-    "r": (221, 0, 38, 255),
-    "g": (0, 191, 48, 255),
-    "h": (64, 64, 64, 255),
-    "o": (244, 149, 4, 255),
-}
-font_syht_m = ImageFont.truetype(
-    os.path.join(FONTS_PATH, "SourceHanSansCN-Normal.otf"), 18
-)
-font_syht_mm = ImageFont.truetype(
-    os.path.join(FONTS_PATH, "SourceHanSansCN-Normal.otf"), 24
-)
-font_syht_ml = ImageFont.truetype(
-    os.path.join(FONTS_PATH, "SourceHanSansCN-Normal.otf"), 32
-)
-font_syhtmed_32 = ImageFont.truetype(
-    os.path.join(FONTS_PATH, "SourceHanSansCN-Medium.otf"), 32
-)
-font_syhtmed_24 = ImageFont.truetype(
-    os.path.join(FONTS_PATH, "SourceHanSansCN-Medium.otf"), 24
-)
-font_syhtmed_18 = ImageFont.truetype(
-    os.path.join(FONTS_PATH, "SourceHanSansCN-Medium.otf"), 18
-)
-font_hywh_85w = ImageFont.truetype(os.path.join(FONTS_PATH, "汉仪文黑.ttf"), 40)
-font_hywh_85w_xs = ImageFont.truetype(os.path.join(FONTS_PATH, "汉仪文黑.ttf"), 18)
-font_hywh_85w_s = ImageFont.truetype(os.path.join(FONTS_PATH, "汉仪文黑.ttf"), 20)
-font_hywh_85w_mms = ImageFont.truetype(os.path.join(FONTS_PATH, "汉仪文黑.ttf"), 25)
-font_hywh_85w_ms = ImageFont.truetype(os.path.join(FONTS_PATH, "汉仪文黑.ttf"), 30)
-font_hywh_85w_l = ImageFont.truetype(os.path.join(FONTS_PATH, "汉仪文黑.ttf"), 55)
-font_sg_emj = ImageFont.truetype(os.path.join(FONTS_PATH, "seguiemj.ttf"), 55)
-font_sg_emj_l = ImageFont.truetype(os.path.join(FONTS_PATH, "seguiemj.ttf"), 75)
+BACK_CLR = {'r': (255, 232, 236, 255), 'g': (219, 255, 228, 255), 'h': (234, 234, 234, 255), 'o': (254, 232, 199, 255)}
+FONT_CLR = {'r': (221, 0, 38, 255), 'g': (0, 191, 48, 255), 'h': (64, 64, 64, 255), 'o': (244, 149, 4, 255)}
+font_syht_m = ImageFont.truetype(os.path.join(FONTS_PATH, 'SourceHanSansCN-Normal.otf'), 18)
+font_syht_mm = ImageFont.truetype(os.path.join(FONTS_PATH, 'SourceHanSansCN-Normal.otf'), 24)
+font_syht_ml = ImageFont.truetype(os.path.join(FONTS_PATH, 'SourceHanSansCN-Normal.otf'), 32)
+font_syhtmed_32 = ImageFont.truetype(os.path.join(FONTS_PATH, 'SourceHanSansCN-Medium.otf'), 32)
+font_syhtmed_24 = ImageFont.truetype(os.path.join(FONTS_PATH, 'SourceHanSansCN-Medium.otf'), 24)
+font_syhtmed_18 = ImageFont.truetype(os.path.join(FONTS_PATH, 'SourceHanSansCN-Medium.otf'), 18)
+font_hywh_85w = ImageFont.truetype(os.path.join(FONTS_PATH, '汉仪文黑.ttf'), 40)
+font_hywh_85w_xs = ImageFont.truetype(os.path.join(FONTS_PATH, '汉仪文黑.ttf'), 18)
+font_hywh_85w_s = ImageFont.truetype(os.path.join(FONTS_PATH, '汉仪文黑.ttf'), 20)
+font_hywh_85w_mms = ImageFont.truetype(os.path.join(FONTS_PATH, '汉仪文黑.ttf'), 25)
+font_hywh_85w_ms = ImageFont.truetype(os.path.join(FONTS_PATH, '汉仪文黑.ttf'), 30)
+font_hywh_85w_l = ImageFont.truetype(os.path.join(FONTS_PATH, '汉仪文黑.ttf'), 55)
+font_sg_emj = ImageFont.truetype(os.path.join(FONTS_PATH, 'seguiemj.ttf'), 55)
+font_sg_emj_l = ImageFont.truetype(os.path.join(FONTS_PATH, 'seguiemj.ttf'), 75)
