94d7a258e0c8e33604723640d9683f06bbdd1ab7
xiaoyu01 <1241836708@qq.com>
2023-12-21 02:11:40 +0800

    update

--- utils/messageChain.py
+++ utils/messageChain.py
@@ -1,26 +1,21 @@
-from typing import List, Dict, Optional,Any
-import base64
+from typing import List, Dict, Optional, Any
+import re, base64
 from PIL import Image
 from io import BytesIO
-import re
 
-def imgToBase64(imgPath:str)->str:
-    img = Image.open(imgPath)
-    imgData = BytesIO()
-    img.save(imgData)
-    b64img = base64.b64encode(imgData.getvalue()).decode('utf-8')
-    return b64img
 
-def messagePieceQuote(text:str)->str:
-    return text.replace('&','&amp;').replace('[','&#91;').replace(']','&#93;').replace(',','&#44;')
+def messagePieceQuote(text: str) -> str:
+    return text.replace('&', '&amp;').replace('[', '&#91;').replace(']', '&#93;').replace(',', '&#44;')
 
-def messagePieceToCqcode(messagePiece:Dict[str,Any]):
+
+def messagePieceToCqcode(messagePiece: Dict[str, Any]):
     if messagePiece['type'] == 'text':
         return messagePiece['data']['text']
-    return '[CQ:%s,%s]'%(messagePiece['type'],','.join(['%s=%s'%(
-        messagePieceQuote(k),messagePieceQuote(v)) for k,v in messagePiece['data'].items()]))
+    return '[CQ:%s,%s]' % (messagePiece['type'], ','.join(['%s=%s' % (
+        messagePieceQuote(k), messagePieceQuote(v)) for k, v in messagePiece['data'].items()]))
 
-def cqcodeToMessagePiece(cqcode:str)->Optional[Dict[str,Any]]:
+
+def cqcodeToMessagePiece(cqcode: str) -> Optional[Dict[str, Any]]:
     cqcodePattern = re.compile(r'^\[(CQ\:[^\[\]\s]+)\]$')
     cqtypePattern = re.compile(r'^CQ\:([^\[\]\s\,]+)$')
     result = cqcodePattern.findall(cqcode)
@@ -28,7 +23,7 @@ def cqcodeToMessagePiece(cqcode:str)->Optional[Dict[str,Any]]:
         return None
     result = result[0].split(',')
     cqtype = cqtypePattern.findall(result[0])
-    if len(cqtype) == 0: 
+    if len(cqtype) == 0:
         return None
     cqtype = cqtype[0]
     cqdict = {}
@@ -43,36 +38,27 @@ def cqcodeToMessagePiece(cqcode:str)->Optional[Dict[str,Any]]:
         'data': cqdict,
     }
 
-def imgToBase64(imgPath:str)->str:
+
+def imgToBase64(imgPath: str) -> str:
     img = Image.open(imgPath)
     imgData = BytesIO()
     img.save(imgData, format=img.format)
     b64img = base64.b64encode(imgData.getvalue()).decode('utf-8')
     return b64img
 
+
 class MessageChain():
     supportedCqcodes = [
-        'text', 'image', 'face', 
+        'text', 'image', 'face',
         'forward', 'node'
-    ]# for Lagrange.Core
+    ]  # for Lagrange.Core
     cqPattern = re.compile(r'(\[CQ\:[^\[]*\])')
-    def __init__(self, chain:List[Dict[str,Any]]) -> None:
-        self.chain:List[Dict[str,Any]] = chain
-        
-    def convertImgPathToBase64(self):
-        result = []
-        for piece in self.chain:
-            if piece['type'].lower() == 'image':
-                path:str = piece['data'].get('file', '')
-                if path.startswith('file:///'):
-                    path = path[len('file:///'):]
-                    b64 = imgToBase64(path)
-                    piece['data']['file'] = 'base64://'+b64
-            result.append(piece)
-        self.chain = result
-    
+
+    def __init__(self, chain: List[Dict[str, Any]]) -> None:
+        self.chain: List[Dict[str, Any]] = chain
+
     @classmethod
-    def fromCqcode(cls, cqcode:str):
+    def fromCqcode(cls, cqcode: str):
         pieces = cls.cqPattern.split(cqcode)
         result = []
         for idx, text in enumerate(pieces):
@@ -89,14 +75,14 @@ class MessageChain():
                 }
             result.append(piece)
         return cls(result)
-    
-    def toCqcode(self)->str:
+
+    def toCqcode(self) -> str:
         result = []
         for piece in self.chain:
             result.append(messagePieceToCqcode(piece))
         return ''.join(result)
-    
-    def supportForLagrange(self)->bool:
+
+    def supportForLagrange(self) -> bool:
         if not isinstance(self.chain, list):
             return False
         for item in self.chain:
@@ -114,6 +100,19 @@ class MessageChain():
             if piece['type'].lower() in self.supportedCqcodes:
                 result.append(piece)
         self.chain = result
+
+    def convertImgPathToBase64(self):
+        result = []
+        for piece in self.chain:
+            if piece['type'].lower() == 'image':
+                path: str = piece['data'].get('file', '')
+                if path.startswith('file:///'):
+                    path = path[len('file:///'):]
+                    b64 = imgToBase64(path)
+                    piece['data']['file'] = 'base64://' + b64
+            result.append(piece)
+        self.chain = result
+
 
 if __name__ == '__main__':
     testCases = [
